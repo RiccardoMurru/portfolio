ed7d271f911fdddbee9ff99d15f02a0b4f6d6f40
Riccardo Murru <riccardo.mr@gmail.com>
2020-08-24 19:34:10 +0200

    minor fixes

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,6 @@
 		<HeaderComponent />
 		<router-view />
 		<FooterComponent />
-		<!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/> -->
 	</div>
 </template>
 
